28d91a1a9d4d10f5f83b44d34252ef1c9df96150
Marcio de Freitas Mendes Junior <marciofmjr@hotmail.com>
2019-05-07 01:07:16 -0300

    Update scroll-to-first-photo.js

--- instagram/scroll-to-first-photo.js
+++ instagram/scroll-to-first-photo.js
@@ -4,26 +4,20 @@ var SPEED = 1000;
 var lastBodyHeight = 0;
 
 function getBodyHeight() {
-	var body = document.body;
+    var body = document.body;
     var html = document.documentElement;
     var height = Math.max( body.scrollHeight, body.offsetHeight, 
                            html.clientHeight, html.scrollHeight, html.offsetHeight );
-	return height;
+    return height;
 }
 
 
 var interval = setInterval(function() {
-	console.log("chamou set interval");
-
 	var actualBodyHeight = getBodyHeight();
-
-	console.log("last: " + lastBodyHeight + " , actual: " + actualBodyHeight);
-
 	if(lastBodyHeight == actualBodyHeight) {
 		clearInterval(interval);
 	} else {
 		lastBodyHeight = actualBodyHeight;
 		window.scrollTo(0,document.body.scrollHeight);
-	}
-	
+	}	
 }, SPEED);
